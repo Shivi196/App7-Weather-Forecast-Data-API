ed180e027758ede49edddedc9ca46701d782ac68
misha <misha07.sharma@gmail.com>
2025-02-18 22:58:58 +0530

    Day33: Add Sky conditions with the images and change temperature in decimal value and using try except to avoid place doesn't exist error code

--- backend.py
+++ backend.py
@@ -2,21 +2,17 @@ import requests
 
 API_key = "f815b29bdd8da50d79e2f0a124cdda5e"
 
-def get_data(place,forecast_days=None,kind=None):
+def get_data(place,forecast_days=None):
   url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
   response = requests.get(url)
   data = response.json()
   filtered_data = data["list"]
   nr_values = 8 * forecast_days #this api generate report every 3hr in 24hr/1day that's 8 times in single day so if you want to forecast data for week so type in forecast_days as 5 and it will give 8 * 5 = 40 observations
-  filtered_data = filtered_data[:nr_values]
-  if kind == "Temperature":
-      filtered_data = [dict["main"]["temp"] for dict in filtered_data] #check for list comprehension topic for this to understand
-  if kind == "Sky":
-      filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+  filtered_data = filtered_data[:nr_values] #filtering the list values with the no of forecasted days entered by user
 
   return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo",forecast_days=3,kind="Sky"))
+    print(get_data(place="Tokyo",forecast_days=3))
 
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
-st.title("Weather Forecast for the Next Days")
 
+# Add title,text_input,slider,selectbox & subheader
+st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days",min_value=1,max_value=5,help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",options=("Temperature","Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place: #This means if place is not null and user entered value there then only run below code otherwise not
 
-d,t = get_data(days)
+    try: #Why use try except :  in order to replace the error when place doesn't exist with the keyerror instead showing the original error code to user
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x":"Dates","y":"Temperatures(C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperature_data = [dict["main"]["temp"] / 10 for dict in filtered_data]  # check for list comprehension topic for this to understand
+            date = [dict["dt_txt"] for dict in filtered_data]
+
+            # Create a temperature plot
+            figure = px.line(x=date, y=temperature_data, labels={"x":"Dates","y":"Temperatures(C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+
+            images = {"Clouds": "cloud_images/cloud.png",
+                      "Clear": "cloud_images/clear.png",
+                      "Rain":"cloud_images/rain.png",
+                      "Snow": "cloud_images/snow.png"}
+            sky_conditions_data = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions_data]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write(f"Sorry {place} doesn't exist..Try entering different place ")
